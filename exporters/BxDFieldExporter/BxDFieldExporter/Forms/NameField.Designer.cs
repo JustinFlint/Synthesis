173d13fbaac2c0ffb8ec6f897896b5ebe1457641
Lauren Choquer <choquerlauren@gmail.com>
2017-08-24 14:59:02 -0700

    Added forms to synchronize field and robot exporters

---
+++ exporters/BxDFieldExporter/BxDFieldExporter/Forms/NameField.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace BxDFieldExporter.Forms
+{
+    partial class NameFieldForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NameFieldForm));
+            this.lbl_Name = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lbl_Save = new System.Windows.Forms.Label();
+            this.txtDirectory = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.ExportLocationDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // lbl_Name
+            // 
+            this.lbl_Name.AutoSize = true;
+            this.lbl_Name.Location = new System.Drawing.Point(12, 21);
+            this.lbl_Name.Name = "lbl_Name";
+            this.lbl_Name.Size = new System.Drawing.Size(258, 16);
+            this.lbl_Name.TabIndex = 1;
+            this.lbl_Name.Text = "Please enter a name for the exported field:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(12, 39);
+            this.txtName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(543, 22);
+            this.txtName.TabIndex = 2;
+            // 
+            // lbl_Save
+            // 
+            this.lbl_Save.AutoSize = true;
+            this.lbl_Save.Location = new System.Drawing.Point(12, 89);
+            this.lbl_Save.Name = "lbl_Save";
+            this.lbl_Save.Size = new System.Drawing.Size(97, 16);
+            this.lbl_Save.TabIndex = 3;
+            this.lbl_Save.Text = "Save Location:";
+            // 
+            // txtDirectory
+            // 
+            this.txtDirectory.Location = new System.Drawing.Point(115, 85);
+            this.txtDirectory.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtDirectory.Name = "txtDirectory";
+            this.txtDirectory.Size = new System.Drawing.Size(439, 22);
+            this.txtDirectory.TabIndex = 4;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(12, 133);
+            this.btnBrowse.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(160, 30);
+            this.btnBrowse.TabIndex = 5;
+            this.btnBrowse.Text = "Browse...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.BrowseButton_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(205, 133);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(156, 30);
+            this.btnCancel.TabIndex = 6;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(396, 133);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(157, 30);
+            this.btnOK.TabIndex = 7;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.OKButton_Click);
+            // 
+            // NameFieldForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 181);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtDirectory);
+            this.Controls.Add(this.lbl_Save);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lbl_Name);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "NameFieldForm";
+            this.Text = "Name Field";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lbl_Name;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lbl_Save;
+        private System.Windows.Forms.TextBox txtDirectory;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.FolderBrowserDialog ExportLocationDialog;
+    }
+}
---
+++ exporters/BxDFieldExporter/BxDFieldExporter/Forms/NameField.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BxDFieldExporter.Forms
+{
+    public partial class NameFieldForm : Form
+    {
+        public enum NameMode { Initial, SaveAs }
+        private NameMode nameMode;
+
+        public NameFieldForm(NameMode mode)
+        {
+            InitializeComponent();
+
+            nameMode = mode;
+
+            txtDirectory.Text = SynthesisGUI.PluginSettings.GeneralSaveLocation;
+        }
+
+        public static DialogResult NameField(out string FieldName, NameMode mode = NameMode.SaveAs)
+        {
+            try
+            {
+                NameFieldForm form = new NameFieldForm(mode);
+                form.ShowDialog();
+                FieldName = form.txtName.Text;
+                return form.DialogResult;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                throw;
+            }
+        }
+
+        private void OKButton_Click(object sender, EventArgs e)
+        {
+            string path = txtDirectory.Text;
+
+            if(txtName.Text != null)
+            {
+                string name = txtName.Text;
+
+                if (File.Exists(SynthesisGUI.PluginSettings.GeneralSaveLocation + "\\" + txtName.Text + @"\skeleton.bxdf") && MessageBox.Show("Overwrite Existing Field?", "Save Field", MessageBoxButtons.YesNo) == DialogResult.No)
+                {
+                    return;
+                }
+
+                DialogResult = DialogResult.OK;
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a name for your field.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            DialogResult warningResult = MessageBox.Show("Are you sure you want to cancel? (All export progress would be lost)",
+                "Cancel", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if ((nameMode == NameMode.Initial && warningResult == DialogResult.Yes) || nameMode == NameMode.SaveAs)
+            {
+                DialogResult = DialogResult.Cancel;
+                Close();
+            }
+        }
+
+        private void BrowseButton_Click(object sender, EventArgs e)
+        {
+            if (ExportLocationDialog.ShowDialog() == DialogResult.OK)
+            {
+                PathTextBox.Text = ExportLocationDialog.SelectedPath;
+                SynthesisGUI.PluginSettings.GeneralSaveLocation = ExportLocationDialog.SelectedPath;
+            }
+        }
+
+        private void ExportLocationDialog_HelpRequest(object sender, EventArgs e)
+        {
+            if (ExportLocationDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtDirectory.Text = ExportLocationDialog.SelectedPath;
+            }
+        }
+    }
+}
